f2e2455b3ab13c31563356f26bb83ebeef540342
Elijah Seed-Arita <elijaharita@gmail.com>
2022-10-14 09:20:25 -0400

    feat: flatfs blockstore merging

---
+++ go.mod
@@ -0,0 +1,52 @@
+module github.com/application-research/bsutil
+
+go 1.19
+
+require (
+	github.com/cheggaaa/pb/v3 v3.1.0
+	github.com/ipfs/go-block-format v0.0.3
+	github.com/ipfs/go-datastore v0.5.0
+	github.com/ipfs/go-ds-flatfs v0.5.1
+	github.com/ipfs/go-ipfs-blockstore v1.2.0
+	github.com/urfave/cli/v2 v2.19.2
+)
+
+require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/alexbrainman/goissue34681 v0.0.0-20191006012335-3fc7a47baff5 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/ipfs/bbloom v0.0.4 // indirect
+	github.com/ipfs/go-cid v0.0.7 // indirect
+	github.com/ipfs/go-ipfs-ds-help v1.1.0 // indirect
+	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
+	github.com/ipfs/go-ipld-format v0.3.0 // indirect
+	github.com/ipfs/go-log v1.0.3 // indirect
+	github.com/ipfs/go-log/v2 v2.0.3 // indirect
+	github.com/ipfs/go-metrics-interface v0.0.1 // indirect
+	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/minio/sha256-simd v0.1.1-0.20190913151208-6de447530771 // indirect
+	github.com/mr-tron/base58 v1.1.3 // indirect
+	github.com/multiformats/go-base32 v0.0.3 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multibase v0.0.3 // indirect
+	github.com/multiformats/go-multihash v0.0.14 // indirect
+	github.com/multiformats/go-varint v0.0.5 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cheggaaa/pb/v3"
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-datastore"
+	flatfs "github.com/ipfs/go-ds-flatfs"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Commands = []*cli.Command{
+		{
+			Name: "merge",
+			Flags: []cli.Flag{
+				&cli.StringSliceFlag{
+					Name:    "input",
+					Aliases: []string{"i"},
+				},
+				&cli.StringFlag{
+					Name:    "output",
+					Aliases: []string{"o"},
+					Value:   "./merged-blockstore",
+				},
+			},
+			Action: cmdMerge,
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+func cmdMerge(ctx *cli.Context) error {
+
+	// Create the output blockstore and make sure it doesn't already exist
+	if err := flatfs.Create(ctx.String("output"), flatfs.NextToLast(3)); err != nil {
+		return fmt.Errorf("could not open output blockstore: %v", err)
+	}
+
+	// Open the output blockstore
+	outputDS, err := flatfs.Open(ctx.String("output"), false)
+	if err != nil {
+		return err
+	}
+
+	output := blockstore.NewBlockstoreNoPrefix(outputDS)
+
+	if len(ctx.StringSlice("input")) == 0 {
+		return fmt.Errorf("at least one input is required")
+	}
+
+	// Open the input blockstores
+	for i, inputPath := range ctx.StringSlice("input") {
+		inputDS, err := flatfs.Open(inputPath, false)
+		if err != nil {
+			return err
+		}
+
+		inputSize, err := dirSize(inputPath)
+		if err != nil {
+			return err
+		}
+
+		input := blockstore.NewBlockstoreNoPrefix(inputDS)
+
+		fmt.Printf("Merging %s... (%d/%d)\n", inputPath, i+1, len(ctx.StringSlice("input")))
+		if err := transferBlocks(ctx.Context, input, output, inputSize); err != nil {
+			return err
+		}
+
+		// Close the input datastore - no sync required since it's only being read from
+		if err := inputDS.Close(); err != nil {
+			fmt.Printf("Failed to close input blockstore %d\n", i)
+		}
+	}
+
+	// Make sure the output is synced before closing
+	if err := outputDS.Sync(ctx.Context, datastore.NewKey("/")); err != nil {
+		return err
+	}
+
+	if err := outputDS.Close(); err != nil {
+		return err
+	}
+
+	fmt.Printf("Finished merging\n")
+
+	return nil
+}
+
+func transferBlocks(
+	ctx context.Context,
+	from blockstore.Blockstore,
+	to blockstore.Blockstore,
+	size int64,
+) error {
+
+	allLMDBKeys, err := from.AllKeysChan(ctx)
+	if err != nil {
+		return fmt.Errorf("could not get all lmdb keys channel: %v", err)
+	}
+
+	var buffer []blocks.Block
+
+	bar := pb.New64(size).Set(pb.Bytes, true).Set(pb.CleanOnFinish, true)
+	bar.Start()
+	defer bar.Finish()
+
+	for cid := range allLMDBKeys {
+		block, err := from.Get(ctx, cid)
+		if err != nil {
+			return fmt.Errorf("could not get expected block '%s' from lmdb blockstore: %v", cid, err)
+		}
+
+		buffer = append(buffer, block)
+		if len(buffer) >= 100 {
+			if err := to.PutMany(ctx, buffer); err != nil {
+				return fmt.Errorf("could not write block '%s' to flatfs blockstore: %v", cid, err)
+			}
+			buffer = nil
+		}
+
+		bar.Add(len(block.RawData()))
+	}
+
+	return nil
+}
+
+func dirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
